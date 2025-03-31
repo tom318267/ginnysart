e18be1b784b10c956e856fe99c3e4db5673990f6
tom318267 <ogar318267@gmail.com>
2025-03-31 19:28:34 -0400

    change email

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script
+          src="//code.tidio.co/i1lvmpztsccejtdcfdyu8aex4lrbqtbe.js"
+          async
+        ></script>
+      </body>
+    </Html>
+  );
+}
